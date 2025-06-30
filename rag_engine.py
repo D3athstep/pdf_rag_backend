90fef563952bcb43c41cdf7dac63e24556fc6db6
Ibrahim <ibrahimkhalilullah799@gmail.com>
2025-06-30 16:20:45 +0500

    embedding with sentence trasnformers

--- rag_engine.py
+++ rag_engine.py
@@ -3,11 +3,13 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import re
 import chromadb
 from chromadb.config import Settings 
-from dotenv import  load_dotenv
-import os 
-from openai import OpenAI
-load_dotenv ()
-client=OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
+#from dotenv import  load_dotenv
+#import os 
+#from openai import OpenAI
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer("all-MiniLM-L6-V2")
+#load_dotenv ()
+#client=OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
 
 def processpdf(pdf_path):
     #get the pdf file and extract the data from it 
@@ -26,7 +28,6 @@ def processpdf(pdf_path):
     return  alltext
 def split_into_chunks(text):
     #data is chunked in this piece of code
-    clean_chunk=[]
     text_splitter= RecursiveCharacterTextSplitter(
         chunk_size= 1000,
         chunk_overlap =200
@@ -37,22 +38,25 @@ def split_into_chunks(text):
         print("chunks",i+1)
         print (chunk)
         print("-" * 50)
-    #white spaces are removed here
-    for chunk in chunks:
-        chunk = chunk.strip()
-    #removed unwanted  text like  \n
-        if len(chunk) <30 :
-            continue
-        #chunk= chunk.replace('\n',' ').replace('\t', ' ')
-        chunk= re.sub(r"[^a-zA-Z0-9.,;:!?()\"\'\s]", "", chunk)
-        if chunk.lower().startswith("o"):
-            continue
-        chunk= " ".join(chunk.split())
-        clean_chunk.append(chunk)
-        print(f"Filtered {len(chunks)} → {len(clean_chunk)} chunks after removing short ones.")
+    return clean_chunks(chunks)
 
+def clean_chunks(chunks):
+        cleaned_chunks=[]
+        for chunk in chunks:
+            chunk = chunk.strip()
+            #removed unwanted  text like  \n
+            if len(chunk) <30 :
+                continue
+            #chunk= chunk.replace('\n',' ').replace('\t', ' ')
+            chunk= re.sub(r"[^a-zA-Z0-9.,;:!?()\"\'\s]", "", chunk)
+            if chunk.lower().startswith("o"):
+                continue
+            chunk= " ".join(chunk.split())
+            cleaned_chunks.append(chunk)
+            print(f"Filtered {len(chunks)} → {len(cleaned_chunks)} chunks after removing short ones.")
+        return cleaned_chunks
         print("Before:", repr(chunk))
-        print("After :", repr(clean_chunk))
+        print("After :", repr(cleaned_chunks))
 
 def get_chroma_collection(name):
     #connect to chroma client locally
@@ -68,34 +72,49 @@ def dummy_chroma_collection(collection,dummy_document):
     dummy_embeddings=[]
     for i,doc in enumerate(dummy_document):
         try:
-            response = client.embeddings.create(input=doc,model="text-embedding-ada-002")
-            embedding = response.data[0].embedding
+            embedding = model.encode(doc)
             dummy_embeddings.append(embedding)
             print(f"Embedded doc{i+1}")
         except Exception as e:
             print("Error in embedding doc", i+1)
             print(e)
             continue
+    return dummy_embeddings
 
 def query_chroma_collection(collection,query_texts):
-    query_embedding=client.embeddings.create(input=query_texts,model="text-embedding-ada-002")
-    vector = response.data[0].embedding
+   # query_embedding=client.embeddings.create(input=query_texts,model="text-embedding-ada-002")
+    vector = model.encode(query_texts)
     results= collection.query(
-        query_embedding=[vector],n_results=3
+        query_embeddings=[vector],n_results=3
     )
     print("Query result")
     for result in results['documents']:
         print(result)
 
-def embed_and_store(chunks,collection,documents,ids,embeddings):
+def generate_ids(chunks):
+    if chunks is None:
+        print("Chunk is none-  Check  split_into_chunks funtions")
+        return
+    return [f"chunk_{i}" for i in range(len(chunks))]
+
+def embed_chunks(chunks,model):
+    embeddings=[]
+
+    for chunk in chunks:
+        embedding= model.encode(chunk)
+        embeddings.append(embedding)
+    return embeddings
+
+def embed_and_store(collection,chunks,ids,embeddings):
     #dummy_document=["This is a sample document.", "ChromaDB is awesome!", "AI models are fun."]
    # dummy_id=["doc1","doc2","doc3"]
     print("adding data into chroma")
     collection.add(
-    documents= dummy_document,
-    embeddings=dummy_embeddings,
-    ids=dummy_id)
+    documents= chunks,
+    embeddings=embeddings,
+    ids=ids)
     print("added data successfully")
+    print(f"Collection now has {collection.count()} documents.")
 
 #Add and learn data cleaning
 #Embedding it with gemni or chroma
@@ -103,8 +122,12 @@ def embed_and_store(chunks,collection,documents,ids,embeddings):
 if __name__ == "__main__":
     pdf_file = "E:\\AI learning\\pdf-rag-backend\\pdf\\sample.pdf"
     collection= get_chroma_collection("pdf_chunks")
+    alltext=processpdf(pdf_file)
+    chunks= split_into_chunks(alltext)
+    ids=generate_ids(chunks)
+    embeddings=embed_chunks(chunks,model)
     dummy_chroma_collection(collection,dummy_document)
-    query_chroma_collection(collection,"What is ChromaDB")
-   # alltext=processpdf(pdf_file)
-  #  chunks= split_into_chunks(alltext)
+    embed_and_store(collection=collection,chunks=chunks,ids=ids,embeddings=embeddings)
+    query_chroma_collection(collection,"What is the purpose of this document?")
+    
     
